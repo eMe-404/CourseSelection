5aeb142bbdf8fdafd788cbf4552cb35ba7f18d94
yaer dili <yedili@thoughtworks.com>
2019-08-01 16:15:29 +0800

    [yaer] add a health check Controller

---
+++ TASKING.md
@@ -0,0 +1,7 @@
+##Tasking
+###scenario 1 -> 学生注册，登录
+
+#### for sign up
+* **given:** a new student with name, password and email
+* **when**: sign up 
+* **then**: create a new account for the student
---
+++ src/main/java/com/course/courseselection/controllers/HealthCheckController.java
@@ -0,0 +1,13 @@
+package com.course.courseselection.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("/healthCheck")
+    public String healthCheck() {
+        return "the service is active!";
+    }
+}
